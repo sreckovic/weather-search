56848237530844701370cc7f674c5cbcb18a71a0
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-10-06 17:19:31 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+
+import reducers from './reducers';
+
+import './index.css';
+import App from './components/App';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(<App />, document.getElementById('root'));ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
