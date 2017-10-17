25567a1397f768a851890215be418c90422ce562
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-10-17 22:49:49 +1100

    Weather list
    Weather list

---
+++ src/containers/weather_list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    return (
+      <tr key={cityData.city.id}>
+        <td>
+          {cityData.city.name}
+        </td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+/*function mapStateToProps(state) {
+  return { weather: state.weather };
+}*/
+
+function mapStateToProps({ weather }) {
+  return { weather }; // { weather } === { weather: weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
